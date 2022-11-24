721fad8b9b95f1ac84e49ba427f09150170c7feb
justus.taeger <justust@gmx.de>
2022-11-24 09:53:53 +0100

    fixed gene url

--- src/components/queries/UserInput.vue
+++ src/components/queries/UserInput.vue
@@ -187,7 +187,7 @@ export default {
 
                 //http://127.0.0.1:8000/articlebygenelist/?
                 //https://restapi.connect.dzd-ev.de/articlebygenelist/?
-                fetch("http://127.0.0.1:8000/genesbygenelist/?" + this.urlParameters).then((response) => {
+                fetch("https://restapi.connect.dzd-ev.de/genesbygenelist/?" + this.urlParameters).then((response) => {
                     if (response.ok) {
                         // console.log(response);
                         return response.json();
